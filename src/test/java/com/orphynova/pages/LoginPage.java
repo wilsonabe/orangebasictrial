62214512fb819578462f662144d278f90a726dd3
wilsonabe <wilson.abe@gmail.com>
2020-11-04 18:56:56 -0500

    Completed Login Test

--- src/test/java/com/orphynova/pages/LoginPage.java
+++ src/test/java/com/orphynova/pages/LoginPage.java
@@ -3,23 +3,38 @@ package com.orphynova.pages;
 import com.orphynova.lib.PageBase;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
 
 public class LoginPage extends PageBase {
     private String txtUserName ="//*[@id='txtUsername']";
     private String txtPassword = "//*[@id='txtPassword']";
     private String btnLogin = "//*[@id='btnLogin']";
+    private String lblCredentials = "//*[@id='spanMessage']";
 
     public LoginPage(WebDriver driver) {
         super(driver);
     }
 
-    public void Login(String Username, String Password){
-        setText(By.xpath(txtUserName),Username);
-        setText(By.xpath(txtPassword),Password);
+    public HeaderPage Login(String Username, String Password) {
+        setText(By.xpath(txtUserName), Username);
+        setText(By.xpath(txtPassword), Password);
         click(By.xpath(btnLogin));
+        return new HeaderPage(driver);
 
 //    driver.findElement(By.xpath(txtUserName)).sendKeys(Username);
 //    driver.findElement(By.xpath(txtPassword)).sendKeys(Password);
 //    driver.findElement(By.xpath(btnLogin)).click();
-}
+    }
+
+    public void invalidlogin(String Username, String Password, String expectedError){
+            setText(By.xpath(txtUserName),Username);
+            setText(By.xpath(txtPassword),Password);
+            click(By.xpath(btnLogin));
+
+        Assert.assertTrue(isElementPresent(By.xpath(lblCredentials)),"Error not displayed");
+        String actualError = getText(By.xpath(lblCredentials));      // "Invalid credentials"
+
+        Assert.assertEquals(actualError,expectedError,"Incorrect error message");
+
+    }
 }
--- src/test/java/com/orphynova/tests/LoginTest.java
+++ src/test/java/com/orphynova/tests/LoginTest.java
@@ -1,7 +1,6 @@
 package com.orphynova.tests;
 
 import com.orphynova.lib.TestBase;
-import com.orphynova.pages.HeaderPage;
 import com.orphynova.pages.LoginPage;
 import org.testng.annotations.Test;
 
@@ -10,9 +9,16 @@ public class LoginTest extends TestBase {
         @Test
         public void validLoginTest() {
         LoginPage loginpage = new LoginPage(driver);
-        loginpage.Login("admin","admin123");
+        loginpage.Login("Admin","admin123").assertWelcomeMessage();
 
-        new HeaderPage(driver).assertWelcomeMessage();
+      //  new HeaderPage(driver).assertWelcomeMessage();
+        }
+
+        @Test
+        public void invalidLoginTest() {
+                LoginPage loginpage = new LoginPage(driver);
+                loginpage.invalidlogin("admin12","admin","Invalid credentials");
+
         }
 
     }
